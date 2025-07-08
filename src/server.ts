3c94d9e971c18de06af7f2b6a95cd054728d130d
Bryan <mauriciobryan1998@gmail.com>
2025-07-08 19:33:07 -0300

    Creating rooms route

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
     type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from '../env.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -20,4 +21,9 @@ app.get('/health', () => {
     return 'OK';
 });
 
-app.listen({ port: env.PORT });
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT }).then(() => {
+    // biome-ignore lint/suspicious/noConsole: dev
+    console.log(`Server is running on http://localhost:${env.PORT}`)
+});
